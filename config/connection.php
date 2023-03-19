91c912d4ee2063d772f041ae51a22b921d6eaf3b
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-03-19 23:54:18 +0700

    add .gitignore

--- config/connection.php
+++ config/connection.php
@@ -1,15 +1,15 @@
 <?php 
 
-    $databaseHost = 'localhost';
-    $databaseName = 'u5463725_database';
-    $databaseUsername = 'u5463725_database';
-    $databasePassword = 'database2023';
-
-    $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-
     // $databaseHost = 'localhost';
-    // $databaseName = 'database-beasiswa-sup';
-    // $databaseUsername = 'root';
-    // $databasePassword = '';
+    // $databaseName = 'u5463725_database';
+    // $databaseUsername = 'u5463725_database';
+    // $databasePassword = 'database2023';
 
     // $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+    $databaseHost = 'localhost';
+    $databaseName = 'database-beasiswa-sup';
+    $databaseUsername = 'root';
+    $databasePassword = '';
+
+    $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
