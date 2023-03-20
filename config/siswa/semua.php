256f46c7c6bec4927b45d861196fa479f205ccfb
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-03-20 14:14:10 +0700

    update semua.php

--- config/siswa/semua.php
+++ config/siswa/semua.php
@@ -12,13 +12,7 @@
     elseif(isset($_GET['filter_program']) && $_GET['filter_program'] != '') {
 
         $filter_program = $_GET['filter_program'];
-
-        if(isset($_GET['filter']) && $_GET['filter'] != ''){
-            $filter = $_GET['filter'];
-            $siswa_semua = mysqli_query($mysqli, "SELECT * FROM siswa WHERE kabkota = '$filter' ORDER BY id ASC");
-        } else {            
-            $siswa_semua = mysqli_query($mysqli, "SELECT * FROM siswa WHERE program_beasiswa = '$filter_program' ORDER BY id ASC");
-        }
+        $siswa_semua = mysqli_query($mysqli, "SELECT * FROM siswa WHERE program_beasiswa = '$filter_program' ORDER BY id ASC");
         
     } 
 
