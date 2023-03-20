20fa9ada12f6fb82b05ec1944e42e3ebfd0ec307
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-03-20 11:39:00 +0700

    update bg total

--- config/siswa/semua.php
+++ config/siswa/semua.php
@@ -7,7 +7,18 @@
         $filter = $_GET['filter'];
     
         $siswa_semua = mysqli_query($mysqli, "SELECT * FROM siswa WHERE kabkota = '$filter' ORDER BY id ASC");
+        
 
-    } else {
+    } 
+    
+    elseif(isset($_GET['filter_program']) && $_GET['filter_program'] != '') {
+
+        $filter_program = $_GET['filter_program'];
+    
+        $siswa_semua = mysqli_query($mysqli, "SELECT * FROM siswa WHERE program_beasiswa = '$filter_program' ORDER BY id ASC");
+        
+    } 
+
+    else {
         $siswa_semua = mysqli_query($mysqli, "SELECT * FROM siswa ORDER BY id ASC");
     }   
--- config/total/index.php
+++ config/total/index.php
@@ -1,8 +1,25 @@
         <div class="row">
             
             <!-- .col start -->
-            <div class="col-lg-2 py-lg-5 ps-5  text-center">
-                <div class="card shadow">
+            <div class="col-lg-2 py-lg-5 ps-5 text-center">
+                <div class="card shadow <?php if(!isset($_GET['filter_program'])) { echo 'bg-dark text-light'; } ?>">
+                    <div class="card-body">
+                        <?php 
+                            require_once('./config/connection.php');
+                            $rows_semua_siswa = mysqli_query($mysqli, "SELECT * FROM siswa");
+                            $rows_semua_siswa = mysqli_num_rows($rows_semua_siswa);
+                        ?>
+                        <p class="text-uppercase fw-bold">Total Semua Siswa</p>
+                        <h1><span class="fw-bold"><?=$rows_semua_siswa?></span> Siswa</h1>
+                        <a href="./siswa.php" class="btn link-success btn-sm fw-bold w-100" title="Saat ini link belum berfungsi"><i class="fa-solid fa-arrow-right me-2"></i>Tampilkan</a>
+                    </div>
+                </div>
+            </div>
+            <!-- .col end -->
+            
+            <!-- .col start -->
+            <div class="col-lg-2 py-lg-5 ps-5 text-center">
+                <div class="card shadow <?php if(isset($_GET['filter_program']) && $_GET['filter_program'] == 'SUP Vokasi 2020') { echo 'bg-dark text-light'; } ?>">
                     <div class="card-body">
                         <?php 
                             require_once('./config/connection.php');
@@ -11,15 +28,15 @@
                         ?>
                         <p class="text-uppercase fw-bold">SUP Vokasi 2020</p>
                         <h1><span class="fw-bold"><?=$rows_sup_vokasi?></span> Siswa</h1>
-                        <a href="#" class="btn link-success btn-sm fw-bold w-100" title="Saat ini link belum berfungsi"><i class="fa-solid fa-arrow-right me-2"></i>Tampilkan</a>
+                        <a href="?filter_program=SUP Vokasi 2020" class="btn link-success btn-sm fw-bold w-100" title="Saat ini link belum berfungsi"><i class="fa-solid fa-arrow-right me-2"></i>Tampilkan</a>
                     </div>
                 </div>
             </div>
             <!-- .col end -->
             
             <!-- .col start -->
-            <div class="col-lg-2 py-lg-5 ps-5  text-center">
-                <div class="card shadow">
+            <div class="col-lg-2 py-lg-5 ps-5 text-center">
+                <div class="card shadow <?php if(isset($_GET['filter_program']) && $_GET['filter_program'] == 'SUP 2021') { echo 'bg-dark text-light'; } ?>">
                     <div class="card-body">
                         <?php 
                             require_once('./config/connection.php');
@@ -28,7 +45,7 @@
                         ?>
                         <p class="text-uppercase fw-bold">SUP 2021</p>
                         <h1><span class="fw-bold"><?=$rows_sup_2021?></span> Siswa</h1>
-                        <a href="#" class="btn link-success btn-sm fw-bold w-100" title="Saat ini link belum berfungsi"><i class="fa-solid fa-arrow-right me-2"></i>Tampilkan</a>
+                        <a href="?filter_program=SUP 2021" class="btn link-success btn-sm fw-bold w-100" title="Saat ini link belum berfungsi"><i class="fa-solid fa-arrow-right me-2"></i>Tampilkan</a>
                     </div>
                 </div>
             </div>
