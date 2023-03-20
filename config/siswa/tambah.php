d55f18e541aa605c5350ec38e1f87f4297313b90
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-03-20 18:01:05 +0700

    update tambah.php

--- config/siswa/tambah.php
+++ config/siswa/tambah.php
@@ -8,11 +8,11 @@
         $nik = $_POST['nik'];        
         $kabkota = $_POST['kabkota'];        
         $program_beasiswa = $_POST['program_beasiswa'];        
-        $folder_google_drive = $_POST['folder_google_drive'];        
         $lokasi_studi = $_POST['lokasi_studi'];        
+        $folder_google_drive = $_POST['folder_google_drive'];        
         $keterangan = $_POST['keterangan'];        
                 
-        $result = mysqli_query($mysqli, "INSERT INTO siswa(nama_lengkap,nik,kabkota,program_beasiswa,lokasi_studi,keterangan) VALUES('$nama_lengkap','$nik','$kabkota','$program_beasiswa','$keterangan')");
+        $result = mysqli_query($mysqli, "INSERT INTO siswa(nama_lengkap,nik,kabkota,program_beasiswa,lokasi_studi,folder_google_drive,keterangan) VALUES('$nama_lengkap','$nik','$kabkota','$program_beasiswa','$lokasi_studi','$folder_google_drive','$keterangan')");
 
         header("Location: ../../admin-siswa.php");
     }
